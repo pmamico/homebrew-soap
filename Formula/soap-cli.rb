98ca7db210b04d1c8f6aa48de61bb09e56786434
Mico Papp <miklos.papp@guidance.hu>
2023-02-23 16:17:50 +0100

    1.2

--- Formula/soap-cli.rb
+++ Formula/soap-cli.rb
@@ -1,13 +1,14 @@
 class SoapCli < Formula
   desc "SOAP client for command-line"
   homepage "https://github.com/pmamico/soap-cli"
-  url "https://github.com/pmamico/soap-cli.git", tag: "v1.1"
+  url "https://github.com/pmamico/soap-cli.git", tag: "v1.2"
   license ""
   head "https://github.com/pmamico/soap-cli.git", branch: "main"
 
   depends_on "curl"
   depends_on "grep"
   depends_on "xmlstarlet"
+  depends_on "highlight" => :optional
 
   def install
     bin.install "src/soap"
